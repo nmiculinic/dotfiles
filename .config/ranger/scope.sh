e0e1781cc643511ace53150fdf7f289ad7fc99ab
Neven Miculinic <neven.miculinic@gmail.com>
2018-05-01 16:02:12 +0200

    Ranger config update

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -58,11 +58,11 @@ handle_extension() {
             exit 1;;
 
         # PDF
-        pdf)
-            # Preview as text conversion
-            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - && exit 5
-            exiftool "${FILE_PATH}" && exit 5
-            exit 1;;
+        # pdf)
+        #     # Preview as text conversion
+        #     pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - && exit 5
+        #     exiftool "${FILE_PATH}" && exit 5
+        #     exit 1;;
 
         # BitTorrent
         torrent)
@@ -114,14 +114,14 @@ handle_image() {
         #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
         #     exit 1;;
         # PDF
-        # application/pdf)
-        #     pdftoppm -f 1 -l 1 \
-        #              -scale-to-x 1920 \
-        #              -scale-to-y -1 \
-        #              -singlefile \
-        #              -jpeg -tiffcompression jpeg \
-        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-        #         && exit 6 || exit 1;;
+        application/pdf)
+            pdftoppm -f 1 -l 1 \
+                     -scale-to-x 1920 \
+                     -scale-to-y -1 \
+                     -singlefile \
+                     -jpeg -tiffcompression jpeg \
+                     -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+                && exit 6 || exit 1;;
     esac
 }
 
